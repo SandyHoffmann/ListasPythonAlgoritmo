c9dee307a25cb4221a8b00e482b93e6fe2351495
SandyHoffmann <60969091+SandyHoffmann@users.noreply.github.com>
2021-06-29 23:27:35 -0300

    Novas atvdds

--- Lista 5/SH-PCRG-AER-Alg-05-Ex-04.py
+++ Lista 5/SH-PCRG-AER-Alg-05-Ex-04.py
@@ -1,1 +1,14 @@
-c
+def calcular_mediana(n1,n2,n3):
+    lista_num = [n1,n2,n3]
+    lista_num = sorted(lista_num)
+    return lista_num[1]
+
+
+def main():
+    n1 = float(input("Digite n1: "))
+    n2 = float(input("Digite n2: "))
+    n3 = float(input("Digite n3: "))
+    print(calcular_mediana(n1,n2,n3))
+    
+if __name__ == "__main__":
+    main()
