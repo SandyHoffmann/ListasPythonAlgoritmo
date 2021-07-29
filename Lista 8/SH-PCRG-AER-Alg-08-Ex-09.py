bdda3790e3d96674717290cec41ad4b4b83166a3
Sandy Hoffmann <manglesan98@gmail.com>
2021-07-29 17:24:10 -0300

    Terminando lista 8

---
+++ Lista 8/SH-PCRG-AER-Alg-08-Ex-09.py
@@ -0,0 +1,8 @@
+def raiz_quad(n,estimativa=1):
+	if abs(estimativa**2 - n)<= (10**-12):
+		return estimativa
+	else:
+		estimativa2 = (estimativa + (n/estimativa))/2
+		return raiz_quad(n,estimativa2)
+
+print(raiz_quad(25))
